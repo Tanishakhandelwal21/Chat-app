315f938d41b2d21f0ab95bcbb098505832e5c4d4
Tanishakhandelwal21 <tanishakhandelwal.5hy@gmail.com>
2025-08-16 07:51:58 +0530

    .env added in gitignore

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,2 +1,15 @@
 //middleware to protect routes
-
+export const protectRoute = (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+        if (!token) {
+            return res.status(401).json({ success: false, message: "Unauthorized" });
+        }
+        const decoded = verifyToken(token);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
