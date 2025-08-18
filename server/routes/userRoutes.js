59f906fb6f00326a598b8beb21992fc7893914e7
Tanishakhandelwal21 <tanishakhandelwal.5hy@gmail.com>
2025-08-18 06:30:04 +0530

    routes and api endpoints

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  login,
+  signup,
+  checkAuth,
+  updateProfile,
+} from "../controllers/userController";
+import { protectRoute } from "../middleware/auth";
+
+const userRouter = express.Router();
+
+userRouter.post("/signup", signup);
+userRouter.post("/login", login);
+userRouter.get("/check", protectRoute, checkAuth);
+userRouter.put("/update-profile", protectRoute, updateProfile);
+
+export default userRouter;
