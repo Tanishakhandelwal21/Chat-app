bbc4b82743cd599bec794504dc78321c313de009
Tanishakhandelwal21 <tanishakhandelwal.5hy@gmail.com>
2025-08-18 08:29:39 +0530

    Message conrollers , routes and endpoints setup

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import connectDB from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 //create express app and http server
 const app = express();
@@ -16,9 +17,9 @@ app.use(express.json({ limit: "4mb" }));
 //routes setup
 app.use("/api/status", (req, res) => res.send("Server is running"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 //connect to MongoDB
 await connectDB();
-
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
